098b135632df12c849302f83b0d7bc2c71b3670c
BeAce <beaceshimin@gmail.com>
2017-07-19 01:00:38 +0800

    chore(babelrc): 添加babelrc，为下一步hotloader做准备

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,7 @@ module.exports = {
       {
         test: /.js?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react'],
-        },
+        exclude: /node_modules/
       },
     ],
   },
