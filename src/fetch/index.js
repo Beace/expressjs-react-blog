9fe0fb40dd1aa5a58193a3a59e4ecfb8c2f3f27b
BeAce <beaceshimin@gmail.com>
2017-08-11 10:18:23 +0800

    feat(src/fetch): 抽象API请求

---
+++ src/fetch/index.js
@@ -0,0 +1,35 @@
+const HOST = 'http://localhost';
+const NEWS_HOST = 'http://toutiao-ali.juheapi.com/toutiao/index';
+const PORT = '4000';
+
+const URL = `${HOST}:${PORT}`;
+
+/* eslint-disable no-console */
+
+class API {
+  constructor() {
+    this.newsHost = 'http://toutiao-ali.juheapi.com/toutiao/index';
+    this.host = 'http://localhost';
+    this.port = '4000';
+  }
+  get(url, params) {
+    const param = Object.assign({}, params);
+    console.log(param);
+    return fetch(`${URL}/${url}`).then(res => res.json());
+  }
+
+  getNews(type) {
+    const url = this.newsHost;
+    return fetch(`${url}?type=${type}`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'APPCODE a7962ad7dc794ca69e2c71eb2a3cdda3',
+      },
+    }).then(res => res.json());
+  }
+}
+
+const Fetch = new API();
+
+export default Fetch;
