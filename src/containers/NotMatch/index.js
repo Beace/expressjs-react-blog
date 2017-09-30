c63321569aec49d99954d1f64e7f86bc2753ae82
BeAce <beaceshimin@gmail.com>
2017-09-30 16:01:38 +0800

    feat(containers/NotMatch): 提取404页面

---
+++ src/containers/NotMatch/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from 'components/Header';
+
+const NotMatch = () => (
+  <div>
+    <Header />
+    <div className="container text-center">404 NOMATCH</div>
+  </div>
+);
+
+export default NotMatch;
