44dde10ec36cc8d11eeb3d7ee5dfcdb0c5b6329c
BeAce <beaceshimin@gmail.com>
2017-11-15 11:16:30 +0800

    feat(*): 前后端分离

--- server/db/index.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import colors from 'colors';
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/article', { useMongoClient: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log(colors.green('[db] connected width collection articles successed')));
